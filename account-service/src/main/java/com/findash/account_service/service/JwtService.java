dca109615671ea570eb9d76fbd0f8f42800f8cdd
Emanuel Batista <62020783+emanuel-batista@users.noreply.github.com>
2025-10-28 18:55:59 +0000

    feat: implementation of the Jwt service with secret key and expiration time configuration

--- account-service/src/main/java/com/findash/account_service/service/JwtService.java
+++ account-service/src/main/java/com/findash/account_service/service/JwtService.java
@@ -2,7 +2,24 @@ package com.findash.account_service.service;
 
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+
+import org.springframework.beans.factory.annotation.Value;
+
 @Service
 public class JwtService {
+    @Value("${application.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${application.jwt.expiration-ms}")
+    private long expirationTime;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
     
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
 }
